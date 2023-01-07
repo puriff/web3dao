45c8f82f57d3217a35df85e744ae7c52491cb84f
purif <purifavax@protonmail.com>
2023-01-07 20:05:12 +0100

    initial commit

---
+++ simpleNFT/scripts/deploy.js
@@ -0,0 +1,27 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+  const nftContract = await ethers.getContractFactory("NFTee");
+
+  // here we deploy the contract
+  const deployedNFTContract = await nftContract.deploy();
+
+  // wait for the contract to deploy
+  await deployedNFTContract.deployed();
+
+  // print the address of the deployed contract
+  console.log("NFT Contract Address:", deployedNFTContract.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});

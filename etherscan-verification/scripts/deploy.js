312bf5c3c7f7fc1ccfce2c382d5051771f9861ea
purif <purifavax@protonmail.com>
2023-01-17 15:09:22 +0100

    etherscan verification

---
+++ etherscan-verification/scripts/deploy.js
@@ -0,0 +1,32 @@
+const {ethers} = require("hardhat");
+require("dotenv").config({ path: ".env" });
+
+async function main() {
+  const verifyContract = await ethers.getContractFactory("Verify");
+  const deployedVerifyContract = await verifyContract.deploy();
+  await deployedVerifyContract.deployed();
+
+  // print the address of the deployed contract
+  console.log("Verify Contract Address:", deployedVerifyContract.address);
+
+  console.log("Sleeping.....");
+  // Wait for etherscan to notice that the contract has been deployed
+  await sleep(40000);
+
+  // Verify the contract after deploying
+  await hre.run("verify:verify", {
+    address: deployedVerifyContract.address,
+    constructorArguments: [],
+  });
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
